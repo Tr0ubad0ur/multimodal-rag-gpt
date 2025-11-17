329ee679bb399899166194559217483141c45c08
Matthew Vishniakov <matveyv2004@gmail.com>
2025-11-17 23:21:56 +0300

    feat: init logging in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from backend.api.endpoints import router
+#TODO transfer this to another place (maybe)
+import logging
+from utils.log_config import setup_logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
+#
 
 app = FastAPI(
     title="Multimodal RAG Backend",
